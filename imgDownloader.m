3c47f67d83f336243c6be0c5d1407d642c7300c8
onur <kulaksizoglu.onur@gmail.com>
2019-01-07 21:28:37 +0300

    fix error in downloader

--- imgDownloader.m
+++ imgDownloader.m
@@ -1,12 +1,12 @@
 % downloads images required for the project
-
+options = weboptions('Timeout',Inf);
 for i = 1:size(train_urls)
     url = char(train_urls(i));
-    websave(strcat('C:\Users\onur\Desktop\nöral ödevleri\proje\cap-code/trainimg/',num2str(i)), url(url~=' ' & url~=0))
+    websave(strcat('C:\Users\onur\Desktop\nöral ödevleri\proje\cap-code/trainimg/',num2str(i)), url(url~=' ' & url~=0), options)
 end
 
 for i = 1:size(test_urls)
     url = char(test_urls(i));
-    websave(strcat('C:\Users\onur\Desktop\nöral ödevleri\proje\cap-code/testimg/',num2str(i)), url(url~=' ' & url~=0))
+    websave(strcat('C:\Users\onur\Desktop\nöral ödevleri\proje\cap-code/testimg/',num2str(i)), url(url~=' ' & url~=0), options)
 end
 
