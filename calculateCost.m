c56909ad25757550e0f747214a760fcc3eefe2c4
Taha Aksu <tahaaksu01@gmail.com>
2019-01-11 13:18:14 +0300

    FillinPartly filled function bodies

---
+++ calculateCost.m
@@ -0,0 +1,3 @@
+function [cost] = calculateCost(Label,LabelList,targetRows)
+    
+end
--- createLabel.m
+++ createLabel.m
@@ -1,6 +1,9 @@
 function [count,label] = createLabel(photoList,targetRows,learned_weights1,learned_weights2)
     %Photolist is a matrix N x feature_vec_size. It has the feaute vectors
     %for each N photos in the photoList.
+    count = 0;
+    label = '';
+    maxCost = 9999999999;
     N = size(photoList,1);
     labelLists = zeros(N,10);
     for i=1:N
@@ -11,7 +14,26 @@ function [count,label] = createLabel(photoList,targetRows,learned_weights1,learn
         combos = combntns(1:N,i);
         for j= 1:size(combos,1)
             currentComb = combos(j,:);
-            
+            curLabelList = unique(labelLists(currentComb));
+            if isempty(curLabelList)
+                continue;
+            end
+            for k =1:size(curLabelList)
+                %Check here if any non target photo has the label in a
+                %higher rank than the target photos
+                
+                
+                %Code goes here
+                
+                %Calculate Cost
+                [cost] = calculateCost(curLabelList(k),labelLists,targetRows);
+                if cost < max
+                    max = cost;
+                    label = curLabelList(k);
+                    count = i;
+                end
+                
+            end
         end
     end
 end
